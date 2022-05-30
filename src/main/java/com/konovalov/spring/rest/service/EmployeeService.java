e0603516c68349a97dd554368b63992ebd4347eb
AndreiKonovalov <96517761+AndreiKonovalov@users.noreply.github.com>
2022-05-30 14:20:03 +0500

    first commit

---
+++ src/main/java/com/konovalov/spring/rest/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.konovalov.spring.rest.service;
+
+import com.konovalov.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
